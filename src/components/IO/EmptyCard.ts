91a564ac074afa50526caa2a014de53155b01446
Aaron Wright <aaron.c.wright@icloud.com>
2026-03-08 22:34:16 -0500

    Add EmptyCard

---
+++ src/components/IO/EmptyCard.ts
@@ -0,0 +1,16 @@
+import { IO } from '../IO'
+
+export class EmptyCard implements IO {
+
+  raiseIRQ = () => {}
+  raiseNMI = () => {}
+
+  read(address: number): number {
+    return 0
+  }
+
+  write(address: number, data: number): void {}
+  tick(frequency: number): void {}
+  reset(coldStart: boolean): void {}
+
+}
